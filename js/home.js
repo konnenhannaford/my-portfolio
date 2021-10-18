56c76a15bfa6ece95d38a3dd0cb49651fcc03fe5
konnenhannaford <konnen23@gmail.com>
2021-10-19 03:07:02 +1100

    removed excess javascript

--- js/home.js
+++
@@ -1,8 +0,0 @@
-// grab everything we need
-const btn = document.querySelector(".mobile-menu-button");
-const sidebar = document.querySelector(".sidebar");
-
-// add our event listener for the click
-btn.addEventListener("click", () => {
-  sidebar.classList.toggle("-translate-x-full");
-});
